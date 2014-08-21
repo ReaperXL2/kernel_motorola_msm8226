3172f2e2225ac60c7b11b8a034f35d7af0e27327
Ethan <cannondalev2000@gmail.com>
2014-08-20 19:27:00 -0700

    Revert "msm_mpdecision: fix early_suspend compilation"
    This reverts commit 828a82e4d1b3796ab9745dbb1b693b38a55c0574.

--- arch/arm/mach-msm/msm_mpdecision.c
+++ arch/arm/mach-msm/msm_mpdecision.c
@@ -618,11 +618,11 @@ static int msm_mpdec_lcd_notifier_callback(struct notifier_block *this,
 }
 #else
 static void msm_mpdec_early_suspend(struct early_suspend *h) {
-	   schedule_work(&msm_mpdec_suspend_work);
+	   msm_mpdec_suspend();
 }
 
 static void msm_mpdec_late_resume(struct early_suspend *h) {
-	   schedule_work(&msm_mpdec_resume_work);
+	   msm_mpdec_resume();
 }
 
 static struct early_suspend msm_mpdec_early_suspend_handler = {
